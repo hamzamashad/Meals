d2dbca31325b207461e0c98a810d39ed172016bb
hamzamashad <mashadhamza@outlook.com>
2023-08-31 20:47:53 +0300

    navigation bar

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const menuShowBtnTag = 'nav .disp-btns .fa-bars',
+menuCloseBtnTag = 'nav .disp-btns .fa-x'
+nav = 'nav',
+navLinks = 'nav ul li',
+menuTag = 'nav .hide';
+
+
+function hideNav() {
+    const navWidth = $(menuTag).outerWidth();
+    $(nav).animate({left: -navWidth}, 500);
+    $(menuCloseBtnTag).addClass('d-none');
+    $(menuShowBtnTag).removeClass('d-none');
+
+    $(navLinks).animate({top: 300}, 500);
+}
+
+function showNav() {
+    $(nav).animate({left: 0}, 500);
+    $(menuCloseBtnTag).removeClass('d-none');
+    $(menuShowBtnTag).addClass('d-none');
+
+    $(navLinks).animate({top: 0}, 750);
+}
+
+
+$(navLinks).click(function () { 
+    console.log('HII');
+});
+
+$(menuShowBtnTag).click(function() {
+    showNav();
+});
+
+$(menuCloseBtnTag).click(function () { 
+    hideNav();
+});
+
+hideNav();
